c2252cc43db9372bd25d65da68b11064c8eab3d7
Maksym Koval <kovalmaksym@istitutocorni.it>
2024-12-11 09:51:42 +0000

    Upload New File

---
+++ initialScreenScript.js
@@ -0,0 +1,16 @@
+function showRules(){
+    const rules = document.getElementById("rulesBlock");
+    if(rules.style.display === "none"){
+        rules.style.display = "block";
+    } else{
+        rules.style.display = "none";
+    }
+}
+
+function joinGame(){
+    window.location.href = "" ; // name of file
+}
+
+function createGame(){
+    window.location.href = "" ; // name of file
+}
